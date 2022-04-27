9212f86c14465339aeaadd702234ef0e00496709
Thiago Hedz <tsouzapereira@hotmail.com>
2022-04-27 20:00:12 -0300

    att

--- JAVASCRIPT/aula04/modelo.js
+++ JAVASCRIPT/aula04/modelo.js
@@ -4,24 +4,27 @@ function carregar() {
   var title = document.getElementsByTagName('h1')[0]
 
   var data = new Date()
-  
+
   var hora = data.getHours()
   var minute = data.getMinutes()
-  
-  title.innerHTML = `Agora são ${hora}H ${minute}m`
+  var second = data.getSeconds()
 
-  if (hora >= 0 && hora < 12) {
+  title.innerHTML = `Agora são ${hora}H:${minute}m:${second}s`
+
+  if (hora >= 0 && hora <= 6) {
+    img.src = 'madruga.png'
+    document.body.style.background = '#022d7c'
+    msg.innerHTML = '<strong>Boa Madrugada!</strong>'
+  } else if (hora < 12) {
     img.src = 'manha.png'
     document.body.style.background = '#bfddff'
     msg.innerHTML = '<strong>Bom dia!</strong>'
-
   } else if (hora >= 12 && hora <= 18) {
-    img.src = 'tarde.png' 
+    img.src = 'tarde.png'
     document.body.style.background = '#ffdf94'
     msg.innerHTML = '<strong>Boa tarde!</strong>'
-
   } else {
-    img.src = 'noite.png' 
+    img.src = 'noite.png'
     document.body.style.background = '#192114'
     msg.innerHTML = '<strong>Boa noite!</strong>'
   }
