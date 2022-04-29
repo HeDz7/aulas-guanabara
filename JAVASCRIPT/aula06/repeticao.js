2672aa691c1d4f0d74f614bf4ac20602382e4fc5
Thiago Hedz <tsouzapereira@hotmail.com>
2022-04-29 19:14:56 -0300

    exercicios

--- JAVASCRIPT/aula06/repeticao.js
+++ JAVASCRIPT/aula06/repeticao.js
@@ -1,19 +1,16 @@
 var d = 1
-
  while (d <= 5) {
   console.log('Hello World')
   d++
 }
 
 var c = 1
-
 while (c <= 6) {
   console.log(`Passo ${c}`)
   c++
 }
 
 var w = 1
-
 do {
   console.log(`Salve ${w}`)
   w++
